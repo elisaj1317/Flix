adfad64f327dc3c386c173083b2614b804b80ecd
elisaj1317 <41582626+elisaj1317@users.noreply.github.com>
2021-06-23 15:32:52 -0700

    Added movie details

--- Flix/View Controllers/MoviesViewController.m
+++ Flix/View Controllers/MoviesViewController.m
@@ -8,6 +8,7 @@
 #import "MoviesViewController.h"
 #import "MovieCell.h"
 #import "UIImageView+AFNetworking.h"
+#import "DetailsViewController.h"
 
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
 
@@ -17,6 +18,8 @@
 
 @end
 
+
+
 @implementation MoviesViewController
 
 - (void)viewDidLoad {
@@ -25,14 +28,17 @@
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     
+//    finds currently playing movies
     [self fetchMovies];
     
+//    runs fetchMovies when screen pulled up
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
     [self.tableView insertSubview:self.refreshControl atIndex:0];
 }
 
 - (void)fetchMovies {
+//    access API and stores movies in self.movies
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -41,21 +47,22 @@
                NSLog(@"%@", [error localizedDescription]);
            }
            else {
+//               store all data
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
                
                NSLog(@"%@", dataDictionary);
                
+//               store movies
                self.movies = dataDictionary[@"results"];
                for (NSDictionary *movie in self.movies) {
                    NSLog(@"%@", movie[@"title"]);
                }
                
+//               reloads table after movie info fetched
                [self.tableView reloadData];
                
-               // TODO: Get the array of movies
-               // TODO: Store the movies in a property to use elsewhere
-               // TODO: Reload your table view data
            }
+//        resets the refresh circle
         [self.refreshControl endRefreshing];
        }];
     [task resume];
@@ -67,17 +74,22 @@
     
 }
 
+// sets rows in table to number of movies
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.movies.count;
 }
 
+// sets up cells in table
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    cell has MovieCell format
     MovieCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];
     
+//    sets labels
     NSDictionary *movie = self.movies[indexPath.row];
     cell.titleLabel.text = movie[@"title"];
     cell.synopsisLabel.text = movie[@"overview"];
     
+//    sets poster picture
     NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
     NSString *posterURLString = movie[@"poster_path"];
     NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
@@ -91,14 +103,24 @@
     return cell;
 }
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    
+//    Passes selected Movie into DetailsViewController
+    UITableViewCell *tappedCell = sender;
+    NSIndexPath *indexPath = [self.tableView indexPathForCell:tappedCell];
+    NSDictionary *movie = self.movies[indexPath.row];
+    
+    DetailsViewController *detailsViewController = [segue destinationViewController];
+    
+    detailsViewController.movie = movie;
+    
 }
-*/
+
 
 @end
